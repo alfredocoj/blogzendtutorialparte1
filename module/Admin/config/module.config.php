c2eaaa2635a1975660cedd0c0dfd15c842045d7a
dev <dev@fury>
2015-10-02 18:05:19 -0300

    1ª parte do tutorial ZF2 - blog: Hello world!

---
+++ module/Admin/config/module.config.php
@@ -0,0 +1,50 @@
+<?php
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Admin\Controller\Index' => 'Admin\Controller\IndexController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'admin' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/admin',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Admin\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                        'module'        => 'admin'
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                        'child_routes' => array( //permite mandar dados pela url
+                            'wildcard' => array(
+                                'type' => 'Wildcard'
+                            ),
+                        ),
+                    ),
+
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array( //the module can have a specific layout
+        'template_path_stack' => array(
+            'admin' => __DIR__ . '/../view',
+        ),
+    ),
+);
