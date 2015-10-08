2108722f18d2397c1e470c6319ed813efbfc19ed
dev <dev@fury>
2015-10-08 15:52:19 -0300

    module.config.php do modulo BlogParte1 - Configurado controllers e models e também as rotas e definido o uso doctrine.

--- module/BlogParte1/config/module.config.php
+++ module/BlogParte1/config/module.config.php
@@ -1,4 +1,5 @@
 <?php
+namespace BlogParte1;
 
  return array(
       'view_manager' => array(
@@ -6,10 +7,16 @@
              __DIR__ . '/../view',
          ),
      ),
-
+     'service_manager' => array(
+        'invokables' => array(
+            'BlogParte1\Model\PostModel'             => 'BlogParte1\Model\PostModel',
+            'BlogParte1\Model\CommentModel'          => 'BlogParte1\Model\CommentModel',
+        )
+     ),
      'controllers' => array(
          'invokables' => array(
-             'BlogParte1\Controller\Posts' => 'BlogParte1\Controller\PostsController'
+              'BlogParte1\Controller\Posts'              => 'BlogParte1\Controller\PostsController',
+              'BlogParte1\Controller\Comments'           => 'BlogParte1\Controller\CommentsController',
          )
      ),
 
@@ -27,11 +34,45 @@
                      'route'    => '/blogparte1',
                      // Define default controller and action to be called when this route is matched
                      'defaults' => array(
-                         'controller' => 'BlogParte1\Controller\Posts',
-                         'action'     => 'index',
+                          '__NAMESPACE__' => 'BlogParte1\Controller',
+                         'controller'     => 'Posts',
+                         'action'         => 'index',
                      )
-                 )
+                 ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller][/:action][/:id][/:idtwo]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'id'         => '[0-9]+',
+                                'idtwo'      => '[0-9]+',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+
+                ),
              )
          )
-     )
+     ),
+
+    'doctrine' => array(
+      'driver' => array(
+          __NAMESPACE__ . '_driver' => array(
+            'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+            'cache' => 'array',
+            'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+          ),
+          'orm_default' => array(
+            'drivers' => array(
+            __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+          )
+        )
+    )
+  )
  );
