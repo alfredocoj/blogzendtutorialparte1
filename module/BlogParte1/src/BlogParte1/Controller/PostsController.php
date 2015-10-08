95da433f67aa29146719e487c442ce86fa5e65f7
dev <dev@fury>
2015-10-08 15:52:48 -0300

    Finalizada action: create, update e delete.

--- module/BlogParte1/src/BlogParte1/Controller/PostsController.php
+++ module/BlogParte1/src/BlogParte1/Controller/PostsController.php
@@ -6,18 +6,96 @@ namespace BlogParte1\Controller;
 use Core\Controller\ActionController;
 use Zend\View\Model\ViewModel;
 use Core\Util\Constantes;
+use BlogParte1\Entity\Post;
+use BlogParte1\Form\PostForm;
 
 class PostsController extends ActionController
 {
     public function indexAction()
     {
         $posts = $this->getEntityManager()
-                      ->getRepository('Admin\Entity\Post')
+                      ->getRepository('BlogParte1\Entity\Post')
                       ->findAll();
-        //$posts = $this->getService(Constantes::MODEL_POST)->findAll();
+        //$posts = $this->getService(Constantes::MODEL_POSTS)->findAll();
 
         return new ViewModel([
                 'posts'    => $posts,
             ]);
+    }
+
+    public function createAction()
+    {
+        $request = $this->getRequest();
+        $form = new PostForm();
+
+        if ( $request->getPost() ) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()){
+                $post = new Post();
+                $post->exchangeArray($form->getData());
+
+                $this->getService(Constantes::MODEL_POSTS)->save($post);
+
+                return $this->redirect()->toUrl('/blogparte1/posts/index');
+            }
+        }
+
+        return new ViewModel([
+          'form' => $form
+        ]);
+    }
+
+    public function updateAction()
+    {
+        $id = (int) $this->params()->fromRoute('id');
+
+        $request = $this->getRequest();
+        $form = new PostForm();
+        $form->setAttribute('action', '/blogparte1/posts/update');
+
+        if($id){
+            $post = $this->getEntityManager()->find('BlogParte1\Entity\Post', $id);
+            $form->setData($post->getArrayCopy());
+        }
+
+        elseif ( $request->isPost() ) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()){
+                $post = new Post();
+                $post->exchangeArray($form->getData());
+
+                $repository = $this->getEntityManager();
+                $update = $repository->find('BlogParte1\Entity\Post', $post->getId());
+                $update->exchangeArray($post->getArrayCopy());
+                $repository->flush();
+
+                return $this->redirect()->toUrl('/blogparte1/posts/index');
+            }
+        }
+
+        return new ViewModel([
+          'form' => $form
+        ]);
+    }
+
+    public function deleteAction()
+    {
+        $id = (int) $this->params()->fromRoute('id');
+
+        // try get method
+        if ( $id ) {
+            $repository = $this->getEntityManager();
+
+            $delete = $repository->find('BlogParte1\Entity\Post', $id);
+          try{
+              $repository->remove($delete);
+              $repository->flush();
+          } catch(Exception $e){
+              echo $e->getMessage();
+          }
+        }
+        return $this->redirect()->toUrl('/blogparte1/posts/index');
     }
 }
