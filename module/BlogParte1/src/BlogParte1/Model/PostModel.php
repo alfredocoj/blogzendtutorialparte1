eb96bea09fa50d63a192c923f58f7b6131ceaa3d
dev <dev@fury>
2015-10-08 15:55:03 -0300

    Corrigida setEntity de PostModel.

--- module/BlogParte1/src/BlogParte1/Model/PostModel.php
+++ module/BlogParte1/src/BlogParte1/Model/PostModel.php
@@ -11,33 +11,7 @@ class PostModel extends BaseModel
     public function __construct()
     {
 
-        $this->setEntity('Admin\Entity\Post');
-    }
-
-    /**
-     * Retorna os posts em formato apropriado para campos select.
-     *
-     * @return array Os posts.
-     *
-     */
-    public function getPostsToPopuleSelect()
-    {
-
-        $repository = $this->getDbalConnection();
-        $sql = "SELECT
-                    id as id,
-                    title as title
-                FROM
-                    posts
-                ORDER BY
-                    post_date";
-        $posts = $repository->executeQuery($sql)->fetchAll(\PDO::FETCH_CLASS);
-
-        foreach ($posts as $key => $value) {
-            $retorno[$value->id] = $value->title;
-        }
-
-        return $retorno;
+        $this->setEntity('BlogParte1\Entity\Post');
     }
 
     /**
