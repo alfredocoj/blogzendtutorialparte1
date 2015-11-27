3fa64666cd1960dc6a87a5f6f440643d6bac638f
dev <dev@fury>
2015-11-27 13:46:04 -0300

    Criação de ServiceManager chamado de BaseModel.

---
+++ module/Core/src/Core/Model/BaseModel.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Core\Model;
+
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Doctrine\DBAL\DriverManager;
+
+class BaseModel  implements ServiceLocatorAwareInterface
+{
+    protected $services;
+    protected $entityManager;
+
+    protected function getDbalConnection()
+    {
+        $config = new \Doctrine\DBAL\Configuration;
+        $params = $this->getServiceLocator()->get('Config');
+        $params = $params['doctrine']['connection']['orm_default']['params'];
+
+        return DriverManager::getConnection($params, $config);
+    }
+
+
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->services = $serviceLocator;
+    }
+
+    public function getServiceLocator()
+    {
+        return $this->services;
+    }
+
+    /**
+     * Returns a Service
+     *
+     * @param  string $service
+     * @return Service
+     */
+    protected function getService($service)
+    {
+        return $this->get($service);
+    }
+
+    protected function getEntityManager()
+    {
+        if (null === $this->entityManager) {
+            $this->entityManager = $this->getService('Doctrine\ORM\EntityManager');
+        }
+
+        return $this->entityManager;
+    }
+
+    protected function setEntityManager($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function confConnectionDB()
+    {
+        return $this->getServiceLocator()->get('Config')["db"];
+    }
+}
