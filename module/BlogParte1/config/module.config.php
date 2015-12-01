cf653873d31463202d183252c1e2dfc69d458bc3
dev <dev@fury>
2015-12-01 09:28:41 -0300

    Corrigida identação em module.config.php.

--- module/BlogParte1/config/module.config.php
+++ module/BlogParte1/config/module.config.php
@@ -66,17 +66,17 @@ namespace BlogParte1;
      ),
 
     'doctrine' => array(
-      'driver' => array(
-          __NAMESPACE__ . '_driver' => array(
-            'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-            'cache' => 'array',
-            'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
-          ),
-          'orm_default' => array(
-            'drivers' => array(
-            __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-          )
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+                'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
         )
     )
-  )
  );
